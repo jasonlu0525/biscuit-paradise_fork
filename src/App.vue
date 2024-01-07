749a630eaba50c458f84e8b033899aa670f5b102
SamuelTan <guo.yang.1215@hotmail.com>
2024-01-07 15:51:03 +0800

    Chore: Import bootstrap ( 引用自定義的variables.scss ) 到all.scss，接著App.vue import all.scss
    why
    -使自定義樣式能夠順利覆蓋 Bootstrap 的默認樣式
    
    how
    -依照官方文件客製的指引，將scss檔案依序載入到all.scss
    -App.vue 刪除原有import bootstrap.scss , 改為import all.scss
    
    *root.scss 設定的是全域的 CSS 變數 :root ，包含了顏色、文字..
      *BS v5.1 版本以上須載入 root.scss ,因為 utilities 編譯的 class 有使用 root.scss 的變數
    * BS v5.3.2 版本 多了 variables-dark.scss 和 maps.scss ，這兩支檔案的 Sass map 在 _root.scss 編譯會運用到

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
   <router-view/>
 </template>
 
 <style lang="scss">
-@import "~bootstrap/scss/bootstrap";
+@import "./assets/all.scss";
 </style>
