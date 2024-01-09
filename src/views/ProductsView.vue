cae65b09f2c1ff7ac1f2879fa59017881e703a1d
SamuelTan <guo.yang.1215@hotmail.com>
2024-01-09 14:36:55 +0800

    Feat : 透過彈出視窗新增品項（新增商品）
    1.ProductModal內宣告tempProduct來接收外層的資料
    2.Products透過UpdateProduct將內層傳來的參數儲存到外層的tempProduct，並傳到後端資料庫
    3.定義openModal方法彈出視窗，並在彈出視窗的同時清空資料
    4.將外層的tempProduct 傳到內層的tempProduct
    5.內層定義Props,命名Products來接收外層的tempProduct，並預期傳來的資料是object型別，同時預防資料傳遞失敗有提供default值
    6.因每次傳送資料都會有異，這裡可以定義watch來監聽接收外資資料的Products，並將這個外層的資料透過watch寫到內層的tempProduct
    7.內層準備好tempProduct後，準備emit 來觸發外層的updateProduct 將資料往後端資料庫送
    8.最後加上hideModal將視窗關閉以及重新取得更新後的後端資料庫資料
    
    *商品建立API方法為POST,部分屬性為必填
    *邏輯：點擊 新增 按鈕，跳出彈出視窗 ，輸入完內容點擊確認送出 將資料新增上去
    *內層：ProductModal 外層 ：Products
    *props口訣：前內後外（:product=''tempProduct''）
    *emit: 內層：@click="$emit('update-product', tempProduct)
    *emit: 外層：@update-product="updateProduct"
    *內層的tempProduct 會透過外層的updateProduct參數傳送
    *定義watch原因：因為props是單向數據流，不可以直接修改外層的資料

--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-end mt-3">
-    <button class="btn btn-primary" type="button" @click="$refs.productModalComp.showModal()">
+    <button class="btn btn-primary" type="button" @click="openModal">
       增加一個產品
     </button>
   </div>
@@ -19,12 +19,8 @@
       <tr v-for="item in products" :key="item.id">
         <td>{{ item.category}}</td>
         <td>{{ item.title }}</td>
-        <td class="text-right">
-          {{ item.origin_price }}
-        </td>
-        <td class="text-right">
-          {{ item.price }}
-        </td>
+        <td class="text-right">{{ item.origin_price }}</td>
+        <td class="text-right">{{ item.price }}</td>
         <td>
           <span class="text-success" v-if="item.is_enabled">啟用</span>
           <span class="text-muted" v-else>未啟用</span>
@@ -38,7 +34,7 @@
       </tr>
     </tbody>
   </table>
-  <ProductModalComp ref="productModalComp"></ProductModalComp>
+  <ProductModalComp ref="productModalComp" :product="tempProduct" @update-product="updateProduct"></ProductModalComp>
 </template>
 
 <script>
@@ -49,6 +45,7 @@ export default {
     return {
       products: [],
       pagination: {},
+      tempProduct: {},
     };
   },
   components: {
@@ -59,11 +56,26 @@ export default {
       const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/products`;
       this.$http.get(api)
         .then((res) => {
-          console.log('對',res.data);
           if (res.data.success) {
             this.products = res.data.products;
             this.pagination = res.data.pagination;
           }
+        });
+    },
+    openModal() {
+      this.tempProduct = {};
+      const productComponent = this.$refs.productModalComp;
+      productComponent.showModal();
+    },
+    updateProduct(item) {
+      this.tempProduct = item;
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/product`;
+      const productComponent = this.$refs.productModalComp;
+      this.$http.post(api, { data: this.tempProduct })
+        .then((res) => {
+          console.log(res);
+          productComponent.hideModal();
+          this.getProducts();
         });
     },
   },
