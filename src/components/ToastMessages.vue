3f602fffb138377fd7571ed2c8a99bfb7e5c526e
SamuelTan <guo.yang.1215@hotmail.com>
2024-01-11 22:56:18 +0800

    Refactor：減少非必要程式碼

--- src/components/ToastMessages.vue
+++ src/components/ToastMessages.vue
@@ -17,7 +17,7 @@ export default {
   inject: ['emitter'],
   mounted() {
     this.emitter.on('push-message', (message) => {
-      const { style = 'success', title, content } = message;
+      const { style , title, content } = message;
       this.messages.push({ style, title, content });
     });
   },
