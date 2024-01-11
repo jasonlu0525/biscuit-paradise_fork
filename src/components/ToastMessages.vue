800eb7203fb46ee0f0c6f2b2c5f27a9a23d19be3
SamuelTan <guo.yang.1215@hotmail.com>
2024-01-11 22:47:06 +0800

    Feat : 加入錯誤訊息回饋
    why
    在複雜的界面，有成功和錯誤回饋，對使用者相對友善
    
    how
    1.npm install mitt
    2.src下新增methods資料夾，並加入emitter.js( mitt套件 ： 掛在methods讓全域都可使用)
    3.import mitt , 賦予mitt()到變數emmiter，預設匯出emmiter.js
    4.import emitter到dashboard（後台的根元件） ，並加入provide（讓內層都可以使用到外層的功能），在要使用mitt的內層都使用inject來接收
    6.新增toastMessengers元件，主要作為定位使用以及v-for呈現toast列表
    7.在toastMessengers元件的mounted生命週期加入push-message來接受外部傳來的資訊
    將外部提取出來的資訊解構並push到messages
    8.toast.vue套入toast 模版 ，設定每次的開啟delay時長
    9.將products.vue的事件透過emmiter觸發emit事件傳到toastMessages的on事件
    10.在getProducts更新產品新增判斷，成功或失敗都會透過emitter.emit推送訊息，在失敗時會透過join將陣列的方法一筆一筆取出，並加上、逗號
    
    *訊息回饋可以堆疊，即是可以同時有一個以上的訊息回饋
    *訊息通知需在全域使用，所以將toastMessages檔案獨立出來，並使用mitt進行跨元件溝通
    *每個toast都可以有自己的生命週期

---
+++ src/components/ToastMessages.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="toast-container position-absolute pe-3 top-0 end-0">
+    <ToastComp v-for="(msg, key) in messages" :key="key" :msg="msg" />
+  </div>
+</template>
+
+<script>
+import ToastComp from '@/components/ToastComp.vue';
+
+export default {
+  components: { ToastComp },
+  data() {
+    return {
+      messages: [],
+    };
+  },
+  inject: ['emitter'],
+  mounted() {
+    this.emitter.on('push-message', (message) => {
+      const { style = 'success', title, content } = message;
+      this.messages.push({ style, title, content });
+    });
+  },
+};
+</script>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,16 +1,25 @@
 <template>
   <NavbarComp></NavbarComp>
-  <div class="container-fluid">
+  <div class="container-fluid mt-3 position-relative">
+    <ToastMessages></ToastMessages>
     <router-view/>
   </div>
 </template>
 
 <script>
+import emitter from '@/methods/emitter';
+import ToastMessages from '@/components/ToastMessages.vue';
 import NavbarComp from '../components/NavbarComp.vue';
 
 export default {
   components: {
     NavbarComp,
+    ToastMessages,
+  },
+  provide() {
+    return {
+      emitter,
+    };
   },
   created() {
     const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
