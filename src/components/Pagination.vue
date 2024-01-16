533c2d4c5d18142062ee0a39611a5ae0095ec4de
SamuelTan <guo.yang.1215@hotmail.com>
2024-01-16 10:10:29 +0800

    Feat : 新增上下頁功能 , 固定Pagination 位置
    下一頁
    1.在父層點擊下一頁，會將父層的currenPageOuter props到內層的currentPageInside。
    2.內層定義goNext methods 來處理將currentPageInside +1 再emmit到父層
    3.currentPageInside會傳送到父層的getProducts作為參數page，最後透過API 的切換達到下一頁的功能
    
    上一頁
    1.在父層點擊上一頁，會將父層的currenPageOuter props到內層的currentPageInside。
    2.內層定義goPrevmethods 來處理將currentPageInside +1 再emmit到父層
    3.currentPageInside會傳送到父層的getProducts作為參數page，最後透過API 的切換達到下一頁的功能
    
    固定Pagination 位置
    1.加入fixed bottom &my-5

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,8 +1,9 @@
 <template>
-  <nav aria-label="Page navigation example">
+  <!-- bug fixed-bottom 雖然有達到固定在底部的效果，但是當打開開發工具的時候,nav 會往上重疊到list，透過my-5也沒有辦法避免重疊-->
+  <nav aria-label="Page navigation example" class="fixed-bottom my-5">
     <ul class="pagination justify-content-center">
       <li class="page-item">
-        <a class="page-link" href="#" aria-label="Previous">
+        <a class="page-link" href="#" aria-label="Previous" @click.prevent="goPrev(currentPageInside)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
@@ -10,7 +11,7 @@
         <a class="page-link" href="#" @click.prevent="updatePage(page)"> {{ page }} </a>
       </li>
       <li class="page-item">
-        <a class="page-link" href="#" aria-label="Next">
+        <a class="page-link" href="#" aria-label="Next" @click.prevent="goNext(currentPageInside)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
@@ -21,12 +22,20 @@
 <script>
 // :pages="{ 頁碼資訊 }"
 // @emitPages="更新頁面事件"
-export default {
-  props: ['pages'],
+export default { 
+  props: ['pages','currentPageInside'],
   methods: {
     updatePage(page) {
       this.$emit('emit-pages', page);
     },
+    goNext(currentPageInside){
+      currentPageInside +=1;
+      this.$emit('go-Next', currentPageInside);
+    },
+    goPrev(currentPageInside){
+      currentPageInside -=1;
+      this.$emit('go-Prev', currentPageInside);
+    }
   },
 };
 </script>
