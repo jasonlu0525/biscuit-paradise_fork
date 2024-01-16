93cb76823f6daa3e3f30277b328917af1a33381f
SamuelTan <guo.yang.1215@hotmail.com>
2024-01-16 13:03:43 +0800

    Feat : 新增數字千分號 & 時間轉換為一般格式（套用全域）
    *除了產品，訂單和優惠卷也會使用千分號，所以將此功能獨立出來
    
    1.使用toLocaleString轉換格式，並運用app.config.globalProperties全域註冊

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import { createApp } from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import { createApp } from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import Loading from 'vue3-loading-overlay';
 import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
+import { currency } from './methods/formatFilters';
 
 const app = createApp(App)
-app.use(VueAxios, axios)
-app.use(router)
+
+app.config.globalProperties.$formatFilters = {
+  currency,
+};
+
+app.use(VueAxios, axios);
+app.use(router);
 app.component('LoadingComp', Loading);
 
 app.mount('#app')
